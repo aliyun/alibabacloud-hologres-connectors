6b3943dab13184ab6df794208b5d192512a84207
杳天 <zhanggaodi.zgd@alibaba-inc.com>
2021-08-05 20:10:35 +0800

    flink ordergen自定义源表，flink1.12 connector完善

---
+++ hologres-connector-examples/hologres-connector-flink-ordergen/src/main/java/io/hologres/flink/ordergen/PrefectureCity.java
@@ -0,0 +1,29 @@
+package io.hologres.flink.ordergen;
+
+import java.io.Serializable;
+import java.util.List;
+
+/** PrefectureCity. */
+public class PrefectureCity implements Serializable {
+    private String prefectureNameZh;
+    private String prefectureName;
+    private List<City> cities;
+
+    public PrefectureCity(String prefectureNameZh, String prefectureName, List<City> cities) {
+        this.prefectureNameZh = prefectureNameZh;
+        this.prefectureName = prefectureName;
+        this.cities = cities;
+    }
+
+    public List<City> getCities() {
+        return cities;
+    }
+
+    public void setCities(List<City> cities) {
+        this.cities = cities;
+    }
+
+    public String getPrefectureNameZh() {
+        return prefectureNameZh;
+    }
+}
